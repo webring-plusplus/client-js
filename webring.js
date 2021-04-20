942539444a203c46c0d5b7c215ce0341359bdd80
blakehawkins <blake.hawkins.11@gmail.com>
2021-04-20 22:36:53 +0100

    html demo

--- webring.js
+++ webring.js
@@ -1,14 +1,20 @@
 /// webring("google.com", (uri, peers, depth) => console.log(uri, peers, depth) [, depth = 1]);
 const webring = (uri, onsucc, depth = 1) => {
   const request = new XMLHttpRequest();
-  request.open('GET', `https://${uri}/webring++.json`, true);
+  request.open('GET', `//${uri}/webring++.json`, true);
   
   request.onload = () => {
     const status = request.status;
     if (200 <= status < 400) {
       const response = request.response;
-      console.log(`Parsing JSON response ${response}`);
-      const children = JSON.parse(response).links;
+      var children;
+      try {
+        children = JSON.parse(response).links;
+      } catch (_e) {
+        console.log(`webring++: invalid metadata at ${uri} (maybe 404)`);
+        return;
+      }
+
       onsucc(uri, children, depth);
       if (depth > 0) {
         children.forEach((child) => { webring(child, onsucc, depth - 1); });
@@ -19,7 +25,7 @@ const webring = (uri, onsucc, depth = 1) => {
   request.onerror = () => {
     console.warn(`webring error on ${uri}`);
   };
-  
+ 
   request.send();
 }
 
